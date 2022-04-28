e5feb7169fadecf4963c1034e0f16713af116779
Jonathan Maier <>
2022-04-28 09:56:05 +0200

    add templates.json

--- pages/api/templates.js
+++ pages/api/templates.js
@@ -1,23 +1,6 @@
-import content from '/templates.json';
+import content from './templates.json';
 
-export default async function handler(req, res) {
-    console.log(content)
-    let templates = await getTemplates(req.headers.key)
-//     templates.templates.unshift({title: 'test', description: 'this is a test', logo: 'https://upload.wikimedia.org/wikipedia/commons/b/bd/Test.svg'})
-//    console.log(templates)
-    res
-        .status(200)
-        .json(templates)
-}
-const url = "https://212.227.208.220:9443/api/custom_templates ";
-export async function getTemplates(key) {
-    const res = await fetch(url, {
-        method: 'GET',
-        withCredentials: true,
-        headers: {
-            "x-api-key": 'ptr_lD5Jv9Lno8tX5dxE9TxE6Y1Q3vlG8nmmuvo7rJ1dYO4='
-        }
-    })
-    const templates = await res.json()
-    return templates
+export default function getTemplates() {
+    console.log(content.templates)
+    return content.templates;
 }
--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ import {Col} from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import StoreFilter from "../components/StoreFilter";
 import useSWR from 'swr';
+import content from './api/templates.json';
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function Home() {
-    const { data, error } = useSWR('/api/templates', fetcher)
-    if (error) return <div>Failed to load users</div>
-    if (!data) return <div>Loading...</div>
+    const data = content;
+
     return (
         <Container style={{
             padding: '1rem'
@@ -28,15 +28,14 @@ export default function Home() {
                     <Card border="dark" href="http://localhost:3000/test">
                         <Card.Body>
                             < Row xs={1} md={4} className="g-2">
-                                {data.map((container) => (
+                                {data.templates.map((container) => (
                                         <Col  key={container.Title + container.description + container.type}>
                                             <Card>
-                                                <Card.Img variant="bottom" src={container.Logo} fluid="true"/>
+                                                <Card.Img variant="bottom" src={container.logo} fluid="true"/>
                                                 <Card.Body>
-                                                    <Card.Title>{container.Title}</Card.Title>
-                                                    <Card.Text>{container.Description}</Card.Text>
-                                                    <Card.Link href="/wordpress">{container.ports}</Card.Link>
-                                                    <Button href={'http://localhost:3000/' + container.Title}>Install</Button>
+                                                    <Card.Title>{container.title}</Card.Title>
+                                                    <Card.Text>{container.description}</Card.Text>
+                                                    <Button href={'http://localhost:3000/' + container.title}>Install</Button>
                                                 </Card.Body>
                                             </Card>
                                         </Col>
