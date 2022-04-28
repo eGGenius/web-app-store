49f4ab3ff6640c155e219028f1cb73537834b51c
Jonathan Maier <>
2022-04-28 16:19:38 +0200

    add Nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { NextUIProvider } from '@nextui-org/react';
+
 import MyNavbar from '../components/MyNavbar';
 
 function MyApp({ Component, pageProps }) {
   
   return (
-    <>
+    <NextUIProvider>
       <MyNavbar/>
       <Component {...pageProps} />
-    </>
+    </NextUIProvider>
   );
 }
 
