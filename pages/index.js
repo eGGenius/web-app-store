30698070c20caeb9527f25da46de1e02655eb5fc
Jonathan Maier <>
2022-07-22 11:00:22 +0200

    polish the start page

--- pages/index.js
+++ pages/index.js
@@ -37,9 +37,9 @@ export default function Home() {
 
     const products = [
         { "id": "0", "link": "/Kanboard", "name": "Kanboard", "description": "Project Management", "logo": "https://firebasestorage.googleapis.com/v0/b/webappstore-4c0a1.appspot.com/o/kanboard.png?alt=media&token=5de76267-1611-4b47-9cb3-febf7ad7011b" },
-        { "id": "1", "link": "/Nextcloud", "name": "Nextcloud", "description": "Productivity Platform", "logo": "https://firebasestorage.googleapis.com/v0/b/webappstore-4c0a1.appspot.com/o/Nextcloud_Logo.svg.png?alt=media&token=1b3127e5-ba27-4616-8a96-9b9db1427e53" },
-        { "id": "2", "link": "/Code%20Server", "name": "Code Server", "description": "VS Code", "logo": "https://firebasestorage.googleapis.com/v0/b/webappstore-4c0a1.appspot.com/o/vscode.png?alt=media&token=2e631410-ac69-4f40-aea2-5f8a24ba06d8" },
-        { "id": "3", "link": "/Wiki.js", "name": "Wiki.js", "description": "Wiki", "logo": "https://firebasestorage.googleapis.com/v0/b/webappstore-4c0a1.appspot.com/o/wikijs.png?alt=media&token=ef6e07f8-b72b-495e-99fd-46b64e175db3" },
+        { "id": "1", "link": "/Wiki.js", "name": "Wiki.js", "description": "Wiki", "logo": "https://firebasestorage.googleapis.com/v0/b/webappstore-4c0a1.appspot.com/o/wikijs.png?alt=media&token=ef6e07f8-b72b-495e-99fd-46b64e175db3" },
+        { "id": "2", "link": "/LibrePhotos", "name": "LibrePhotos", "description": "Photo Library", "logo": "https://firebasestorage.googleapis.com/v0/b/webappstore-4c0a1.appspot.com/o/librephotos.png?alt=media&token=de397f2c-2816-4b81-8a29-c93b75fb3ec8" },
+        { "id": "3", "link": "/Code%20Server", "name": "Code Server", "description": "VS Code", "logo": "https://firebasestorage.googleapis.com/v0/b/webappstore-4c0a1.appspot.com/o/vscode.png?alt=media&token=2e631410-ac69-4f40-aea2-5f8a24ba06d8" },
         { "id": "4", "link": "/Plex", "name": "Plex", "description": "Personal Media Library", "logo": "https://firebasestorage.googleapis.com/v0/b/webappstore-4c0a1.appspot.com/o/plex.png?alt=media&token=884166c2-7523-4932-839b-349f93db6cb5" },
         { "id": "5", "link": "/Rocket.Chat", "name": "Rocket.Chat", "description": "Web Chat Server", "logo": "https://firebasestorage.googleapis.com/v0/b/webappstore-4c0a1.appspot.com/o/rocketchat.png?alt=media&token=3a6a6cce-2c90-4a02-ae92-01bdd508551a" }
     ]
@@ -91,7 +91,7 @@ export default function Home() {
                                     <i className="pi pi-lock text-4xl text-blue-500"></i>
                                 </span>
                                 <div className="text-900 mb-3 font-medium">End-to-End Encryption</div>
-                                <span className="text-700 text-sm line-height-3">Every WebApp is secured by a trusted certificate and all treffic is encrypted</span>
+                                <span className="text-700 text-sm line-height-3">Every WebApp is secured by a trusted certificate</span>
                             </div>
                             <div className="sm:col-12 md:col-6 lg:col-4 mb-4 px-5">
                                 <span className="p-3 shadow-8 mb-3 inline-block" style={{ borderRadius: '10px' }}>
@@ -104,7 +104,7 @@ export default function Home() {
                                 <span className="p-3 shadow-8 mb-3 inline-block" style={{ borderRadius: '10px' }}>
                                     <i className="pi pi-globe text-4xl text-blue-500"></i>
                                 </span>
-                                <div className="text-900 mb-3 font-medium">Fast & Global Accessibilty</div>
+                                <div className="text-900 mb-3 font-medium">Global Accessibilty</div>
                                 <span className="text-700 text-sm line-height-3">Every WebApp is accessible from all around the world</span>
                             </div>
                             <div className="sm:col-12 md:col-6 lg:col-4 mb-4 px-5">
@@ -112,7 +112,7 @@ export default function Home() {
                                     <i className="pi pi-users text-4xl text-blue-500"></i>
                                 </span>
                                 <div className="text-900 mb-3 font-medium">Collaborative</div>
-                                <span className="text-700 text-sm line-height-3">Use one App for your team, your family or your company</span>
+                                <span className="text-700 text-sm line-height-3">WebApps can be used by multiple users at the same time</span>
                             </div>
                             <div className="sm:col-12 md:col-6 lg:col-4 md:mb-4 mb-0 px-3">
                                 <span className="p-3 shadow-8 mb-3 inline-block" style={{ borderRadius: '10px' }}>
@@ -137,7 +137,7 @@ export default function Home() {
                         <div className="sm:col-12 md:col-5 align-items-center flex md:flex-order-0 flex-order-1">
                             <div className="text-center md:text-left md:pl-4">
                                 <div className="font-bold line-height-2 md:text-left text-4xl ">Collaboration</div>
-                                <p className='md:text-left text-lg font-bold'>Use Nextcloud to manage Your Data</p>
+                                <p className='md:text-left text-lg font-bold'>Use <Link href="/Nextcloud"> Nextcloud</Link> to manage Your Data</p>
                                 <p className='md:text-left text-lg'>Nextcloud delivers tight integration of chat, project management, email, calendar and more. Chat messages can be turned directly into tasks, task deadlines are visible in the calendar, and files can be attached to a task via the share dialog.</p>
                             </div>
                         </div>
@@ -152,7 +152,7 @@ export default function Home() {
                         <div className="sm:col-12 md:col-5 align-items-center flex">
                             <div className="text-center md:text-left md:pl-4 ">
                                 <div className="font-bold line-height-2 md:text-left text-4xl ">Communication</div>
-                                <p className="md:text-left text-lg font-bold">Use Rocket.Chat to let every Conversation flow</p>
+                                <p className="md:text-left text-lg font-bold">Use <Link href="/Rocket.Chat"> Rocket.Chat</Link> to let every Conversation flow</p>
                                 <p className="md:text-left text-lg">Rocket.Chat is the world&apos;s largest open source communcation platform and a great alternative to Slack, because it not only provides a great UI, you can also integrate it in your own App.</p>
                             </div>
                         </div>
@@ -161,7 +161,7 @@ export default function Home() {
                         <div className="sm:col-12 md:col-5 align-items-center flex md:flex-order-0 flex-order-1">
                             <div className="text-center md:text-left md:pl-4">
                                 <div className="font-bold line-height-2 md:text-left text-4xl ">Documentation</div>
-                                <p className='md:text-left text-lg font-bold'>Use Wiki.js to make documentation a joy</p>
+                                <p className='md:text-left text-lg font-bold'>Use <Link href="/Wiki.js">Wiki.js</Link> to make documentation a joy</p>
                                 <p className='md:text-left text-lg'>Wiki.js is the most powerful and extensible open source Wiki software, that lets you write and customize your own Wiki</p>
                             </div>
                         </div>
