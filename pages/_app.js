f7ab5fbd712f0981bc4fa1e903469596e79d5b09
Jonathan Maier <>
2022-08-11 16:09:34 +0200

    add cookies after accept

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CookieConsent from "react-cookie-consent";
+
+import Head from 'next/head';
+import Script from 'next/script'
+import { getCookie } from 'cookies-next';
 
 import { UserContext } from '../lib/context';
 import { useUserData } from '../lib/hooks';
 
-import Head from 'next/head';
 import MenuBar from '../components/MenuBar';
 import { ScrollTop } from 'primereact/scrolltop';
 
@@ -15,10 +19,11 @@ import "primeflex/primeflex.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import '../styles/globals.css'
+import Link from 'next/link';
 
-import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
+  const [consent, setConsent] = useState(getCookie('cookieConsent'));
   const userData = useUserData();
   return (
     <UserContext.Provider value={userData}>
@@ -33,15 +38,21 @@ function MyApp({ Component, pageProps }) {
       <MenuBar />
       <Component {...pageProps} />
       <ScrollTop threshold={200} behavior="smooth" />
-      <Script src="https://www.googletagmanager.com/gtag/js?id=UA-212764482-1" strategy="afterInteractive"/>
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-212764482-1');
-        `}
-      </Script>
+      <CookieConsent location="bottom" buttonText="I accept" cookieName="cookieConsent" style={{ background: "#2B373B", paddingLeft: "1rem", paddingRight: "2rem" }} buttonStyle={{ color: "black" }} expires={150} enableDeclineButton acceptOnScroll={false} onAccept={() => {setConsent(true);}}>
+        This website uses cookies to enhance the user experience.
+        <span> </span>
+        <Link href="/privacy-en" className="underline bold" style={{ paddingLeft: "2rem" }}>Privacy Policy</Link>
+      </CookieConsent>
+
+      {consent === true && (
+        <>
+          <Script src="https://www.googletagmanager.com/gtag/js?id=UA-212764482-1" strategy="afterInteractive" />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {` window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-212764482-1'); `}
+          </Script>
+        </>
+      )}
+
     </UserContext.Provider>
   );
 }
