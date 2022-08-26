e41175b35694b08adf97a953bb8f16ce3abe3590
Jonathan Maier <>
2022-08-26 17:42:27 +0200

    make feedback only visible to users

--- pages/feedback.js
+++ pages/feedback.js
@@ -1,18 +1,31 @@
-import { Card } from "primereact/card"
+import { Card } from "primereact/card";
+import { UserContext } from '../lib/context';
+import { useContext } from 'react';
+
+import SignInOutButton from '../components/SignInOutButton';
 
 export default function FeedbackPage({ }) {
+    const { user } = useContext(UserContext);
     return (
 
         <div className="p-4">
             <Card>
-                <iframe 
-                    src="./feedbackForm.html" 
-                    width="100%"
-                    allow="geolocation"
-                    frameBorder="0"
-                    style={{ minWidth: '100%', height: '4000px', border: 'none', backgroundColor: '#1F2D40' }}
-                    scrolling="no"
-                />
+                {user
+                    ? 
+                    <iframe 
+                        src="./feedbackForm.html" 
+                        width="100%"
+                        allow="geolocation"
+                        frameBorder="0"
+                        style={{ minWidth: '100%', height: '4000px', border: 'none', backgroundColor: '#1F2D40' }}
+                        scrolling="no"
+                    />
+                    :
+                    <>
+                        <h3>Sign in to give Feedback</h3>
+                        <SignInOutButton />
+                    </>
+                }
             </Card>
         </div >
     )
