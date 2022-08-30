5345ef9c10379aeec43908524bd9e01ba0485275
Jonathan Maier <>
2022-08-30 10:16:16 +0200

    add feedback form button

--- pages/feedback.js
+++ pages/feedback.js
@@ -2,6 +2,10 @@ import { Card } from "primereact/card";
 import { UserContext } from '../lib/context';
 import { useContext } from 'react';
 
+import { Button } from "primereact/button";
+
+import Link from 'next/link';  
+
 import SignInOutButton from '../components/SignInOutButton';
 
 export default function FeedbackPage({ }) {
@@ -12,14 +16,13 @@ export default function FeedbackPage({ }) {
             <Card>
                 {user
                     ? 
-                    <iframe 
-                        src="./feedbackForm.html" 
-                        width="100%"
-                        allow="geolocation"
-                        frameBorder="0"
-                        style={{ minWidth: '100%', height: '4000px', border: 'none', backgroundColor: '#1F2D40' }}
-                        scrolling="no"
-                    />
+                    <>
+                        <h3>Please take yourself 2 minutes to fill the Feedback Form </h3>
+                        <h3>Thank you for your help :)</h3>
+                        <Link href="https://form.jotform.com/222351883319357" passHref={true}>
+                            <Button>Feedback Form</Button>
+                        </Link>
+                    </>
                     :
                     <>
                         <h3>Sign in to give Feedback</h3>
